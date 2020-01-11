24936b58f7190c85720b2615a52f12b848c751ce
Aaron Prather <34904535+aaprather@users.noreply.github.com>
2020-01-11 10:01:14 -0500

    Created Authorization class, this will be used for storing the token on successful login

---
+++ src/main/java/org/prathdev/accord/domain/Authorization.java
@@ -0,0 +1,16 @@
+package org.prathdev.accord.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Authorization {
+	
+	@JsonProperty("token")
+	private String token = "";
+	
+	public String getToken() {
+		return token;
+	}
+	
+}
