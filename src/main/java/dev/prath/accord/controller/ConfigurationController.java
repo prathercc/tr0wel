5b552f04bbf34d218e70e6a4f903db4d45155346
Aaron Prather <34904535+aaprather@users.noreply.github.com>
2020-01-12 17:48:53 -0500

    Add comments organizing the Channel and Conversation functions. Add code to the toggleControls method that prevents the channel selection box from enabling when the guild selection box has a null value

--- src/main/java/dev/prath/accord/controller/ConfigurationController.java
+++ src/main/java/dev/prath/accord/controller/ConfigurationController.java
@@ -46,24 +46,25 @@ public class ConfigurationController {
 	private Text configProgressText;
 	@FXML
 	private ChoiceBox<Conversation> dmUserDropDown;
-	@FXML 
+	@FXML
 	private Button manageDmButton;
-	
+
 	Properties properties = new Properties();
 
 	private DiscordAccount discordAccount = null;
-	
-	
+
 	public void setUpConfigurationMenu(DiscordAccount val) {
 		discordAccount = val;
 		for (Guild guild : discordAccount.getGuilds()) {
 			guildSelectionBox.getItems().add(guild);
 		}
-		for(Conversation convo: discordAccount.getConversations()) {
+		for (Conversation convo : discordAccount.getConversations()) {
 			dmUserDropDown.getItems().add(convo);
 		}
 	}
-	
+
+	/* Conversation */
+
 	private void launchManageConversation(List<Message> conversationMessages) {
 		Platform.runLater(new Runnable() {
 			@Override
@@ -91,33 +92,6 @@ public class ConfigurationController {
 		});
 	}
 
-	private void launchManageChannel(List<Message> channelMessages) {
-		Platform.runLater(new Runnable() {
-			@Override
-			public void run() {
-				Channel selectedChannel = (Channel) channelSelectionBox.getValue();
-				selectedChannel.setMessages(channelMessages);
-				try {
-					String fxml = "/fxml/manageChannelMenu.fxml";
-					FXMLLoader loader = new FXMLLoader();
-					loader.setLocation(getClass().getResource(fxml));
-					Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxml));
-					Scene scene = new Scene(rootNode);
-					Stage stage = new Stage();
-					ManageChannelController controller = loader.getController();
-					controller.setUpMessageData(discordAccount, selectedChannel);
-					stage.setTitle("accord - Channel Manager Menu");
-					stage.setScene(scene);
-					stage.setResizable(false);
-					stage.initModality(Modality.WINDOW_MODAL);
-					stage.initOwner(AuthenticationController.configurationStage);
-					stage.show();
-				} catch (Exception e) {
-				}
-			}
-		});
-	}
-	
 	public void manageConversation() {
 		try {
 			Thread thread = new Thread(getNewConversationTask());
@@ -128,85 +102,10 @@ public class ConfigurationController {
 		}
 	}
 
-	public void manageChannel() {
-		try {
-			Thread thread = new Thread(getNewChannelTask());
-			thread.setDaemon(true);
-			thread.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	@SuppressWarnings("unchecked")
-	public void selectGuild() {
-		channelSelectionBox.setDisable(false);
-		channelSelectionBox.getItems().clear();
-		channelSelectionBox.getSelectionModel().clearSelection();
-		Guild selectedGuild = (Guild) guildSelectionBox.getValue();
-		for (Channel channel : selectedGuild.getChannels()) {
-			channelSelectionBox.getItems().add(channel);
-		}
-		manageChannelButton.setDisable(true); // New guild was selected, so disable the 'manageChannelButton'
-	}
-
-	public void selectChannel() {
-		manageChannelButton.setDisable(false);
-	}
-	
 	public void selectConversation() {
 		manageDmButton.setDisable(false);
 	}
 
-	private Task<Void> getNewChannelTask() {
-		Task<Void> messageTask = new Task<Void>() {
-			@Override
-			protected Void call() throws Exception {
-				toggleControls(true);
-				Channel selectedChannel = (Channel) channelSelectionBox.getValue();
-				boolean reachedEnd = false;
-				String lastId = "";
-				List<Message> retList = new ArrayList<Message>();
-
-				while (reachedEnd != true) {
-					try {
-						RestTemplate restTemplate = new RestTemplate();
-						String requestUrl = lastId.length() < 1
-								? properties.getDiscordChannelsUrl() + "/" + selectedChannel.getId() + "/messages?limit=100"
-								: properties.getDiscordChannelsUrl() + "/" + selectedChannel.getId() + "/messages?limit=100&before="
-										+ lastId;
-						HttpHeaders headers = new HttpHeaders();
-						headers.set("authorization", discordAccount.getAuthorization());
-						headers.set("user-agent",properties.getUserAgent());
-						HttpEntity<JsonNode> request = new HttpEntity<JsonNode>(headers);
-						restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-						ResponseEntity<Message[]> response = restTemplate.exchange(requestUrl, HttpMethod.GET, request,
-								Message[].class);
-						Message[] responseArr = response.getBody();
-						if (responseArr.length < 100) {
-							updateConfigProgress("Completed loading for channel " + selectedChannel.getId());
-							reachedEnd = true; // If the data length was less than 100, we know we have reached the end
-						}
-						for (Message msg : responseArr) {
-							retList.add(msg); // Populate our retList with the additional data
-						}
-						Thread.sleep(250);
-						lastId = responseArr[responseArr.length - 1].getId(); // Save the last id we are on
-						updateConfigProgress("Loading - [" + lastId + "]");
-					} catch (Exception e) {
-						updateConfigProgress("[" + lastId + "] - Failure!");
-					}
-				}
-				updateConfigProgress("");
-				toggleControls(false);
-				launchManageChannel(retList);
-				return null;
-			}
-		};
-		return messageTask;
-	}
-	
-	
 	private Task<Void> getNewConversationTask() {
 		Task<Void> messageTask = new Task<Void>() {
 			@Override
@@ -221,12 +120,13 @@ public class ConfigurationController {
 					try {
 						RestTemplate restTemplate = new RestTemplate();
 						String requestUrl = lastId.length() < 1
-								? properties.getDiscordChannelsUrl() + "/" + selectedConversation.getId() + "/messages?limit=100"
-								: properties.getDiscordChannelsUrl() + "/" + selectedConversation.getId() + "/messages?limit=100&before="
-										+ lastId;
+								? properties.getDiscordChannelsUrl() + "/" + selectedConversation.getId()
+										+ "/messages?limit=100"
+								: properties.getDiscordChannelsUrl() + "/" + selectedConversation.getId()
+										+ "/messages?limit=100&before=" + lastId;
 						HttpHeaders headers = new HttpHeaders();
 						headers.set("authorization", discordAccount.getAuthorization());
-						headers.set("user-agent",properties.getUserAgent());
+						headers.set("user-agent", properties.getUserAgent());
 						HttpEntity<JsonNode> request = new HttpEntity<JsonNode>(headers);
 						restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 						ResponseEntity<Message[]> response = restTemplate.exchange(requestUrl, HttpMethod.GET, request,
@@ -254,29 +154,151 @@ public class ConfigurationController {
 		};
 		return messageTask;
 	}
+
+	/************/
+	/************/
+	/************/
+	/************/
+
+	/* Channel */
+
+	private void launchManageChannel(List<Message> channelMessages) {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				Channel selectedChannel = (Channel) channelSelectionBox.getValue();
+				selectedChannel.setMessages(channelMessages);
+				try {
+					String fxml = "/fxml/manageChannelMenu.fxml";
+					FXMLLoader loader = new FXMLLoader();
+					loader.setLocation(getClass().getResource(fxml));
+					Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxml));
+					Scene scene = new Scene(rootNode);
+					Stage stage = new Stage();
+					ManageChannelController controller = loader.getController();
+					controller.setUpMessageData(discordAccount, selectedChannel);
+					stage.setTitle("accord - Channel Manager Menu");
+					stage.setScene(scene);
+					stage.setResizable(false);
+					stage.initModality(Modality.WINDOW_MODAL);
+					stage.initOwner(AuthenticationController.configurationStage);
+					stage.show();
+				} catch (Exception e) {
+				}
+			}
+		});
+	}
+
+	public void manageChannel() {
+		try {
+			Thread thread = new Thread(getNewChannelTask());
+			thread.setDaemon(true);
+			thread.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void selectChannel() {
+		manageChannelButton.setDisable(false);
+	}
+
+	@SuppressWarnings("unchecked")
+	public void selectGuild() {
+		channelSelectionBox.setDisable(false);
+		channelSelectionBox.getItems().clear();
+		channelSelectionBox.getSelectionModel().clearSelection();
+		Guild selectedGuild = (Guild) guildSelectionBox.getValue();
+		for (Channel channel : selectedGuild.getChannels()) {
+			channelSelectionBox.getItems().add(channel);
+		}
+		manageChannelButton.setDisable(true); // New guild was selected, so disable the 'manageChannelButton'
+	}
+
+	private Task<Void> getNewChannelTask() {
+		Task<Void> messageTask = new Task<Void>() {
+			@Override
+			protected Void call() throws Exception {
+				toggleControls(true);
+				Channel selectedChannel = (Channel) channelSelectionBox.getValue();
+				boolean reachedEnd = false;
+				String lastId = "";
+				List<Message> retList = new ArrayList<Message>();
+
+				while (reachedEnd != true) {
+					try {
+						RestTemplate restTemplate = new RestTemplate();
+						String requestUrl = lastId.length() < 1
+								? properties.getDiscordChannelsUrl() + "/" + selectedChannel.getId()
+										+ "/messages?limit=100"
+								: properties.getDiscordChannelsUrl() + "/" + selectedChannel.getId()
+										+ "/messages?limit=100&before=" + lastId;
+						HttpHeaders headers = new HttpHeaders();
+						headers.set("authorization", discordAccount.getAuthorization());
+						headers.set("user-agent", properties.getUserAgent());
+						HttpEntity<JsonNode> request = new HttpEntity<JsonNode>(headers);
+						restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+						ResponseEntity<Message[]> response = restTemplate.exchange(requestUrl, HttpMethod.GET, request,
+								Message[].class);
+						Message[] responseArr = response.getBody();
+						if (responseArr.length < 100) {
+							updateConfigProgress("Completed loading for channel " + selectedChannel.getId());
+							reachedEnd = true; // If the data length was less than 100, we know we have reached the end
+						}
+						for (Message msg : responseArr) {
+							retList.add(msg); // Populate our retList with the additional data
+						}
+						Thread.sleep(250);
+						lastId = responseArr[responseArr.length - 1].getId(); // Save the last id we are on
+						updateConfigProgress("Loading - [" + lastId + "]");
+					} catch (Exception e) {
+						updateConfigProgress("[" + lastId + "] - Failure!");
+					}
+				}
+				updateConfigProgress("");
+				toggleControls(false);
+				launchManageChannel(retList);
+				return null;
+			}
+		};
+		return messageTask;
+	}
+
+	/*******/
+	/*******/
+	/*******/
+	/*******/
+	/*******/
 	
 
-	private void toggleControls(boolean val) {		
-		if(!val && channelSelectionBox.getValue() != null) {
-			manageChannelButton.setDisable(val); // Only reenable if there is a channel selected
+	/* Util */
+
+	private void toggleControls(boolean val) {
+		/* Channel Controls */
+		if (!val && channelSelectionBox.getValue() != null) {
+			manageChannelButton.setDisable(val); // Only re-enable if there is a channel selected
 		}
-		if(val) {
+		if (val) {
 			manageChannelButton.setDisable(val);
+			channelSelectionBox.setDisable(val);
+		}
+		if(!val && guildSelectionBox.getValue() != null) {
+			channelSelectionBox.setDisable(val);
 		}
 		
-		channelSelectionBox.setDisable(val);
 		guildSelectionBox.setDisable(val);
-		
-		if(!val && dmUserDropDown.getValue() != null) {
-			manageDmButton.setDisable(val); // Only reenable if there is a conversation selected
+		/*****************/
+
+		/* Conversation Controls */
+		if (!val && dmUserDropDown.getValue() != null) {
+			manageDmButton.setDisable(val); // Only re-enable if there is a conversation selected
 		}
-		if(val) {
+		if (val) {
 			manageDmButton.setDisable(val);
 		}
 		dmUserDropDown.setDisable(val);
-		
+		/**********************/
 	}
-	
 
 	private void updateConfigProgress(String val) {
 		Platform.runLater(new Runnable() {
@@ -286,4 +308,7 @@ public class ConfigurationController {
 			}
 		});
 	}
+
+	/**/
+
 }
