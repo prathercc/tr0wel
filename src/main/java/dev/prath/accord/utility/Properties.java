b88aa179ca4932befd91326810fed523992e7949
Aaron Prather <34904535+aaprather@users.noreply.github.com>
2020-01-13 20:01:22 -0500

    Add the ini path to Properties

--- src/main/java/dev/prath/accord/utility/Properties.java
+++ src/main/java/dev/prath/accord/utility/Properties.java
@@ -1,5 +1,8 @@
 package dev.prath.accord.utility;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Properties {
 	
 	private String discordUsersUrl = "https://discordapp.com/api/users";
@@ -7,6 +10,12 @@ public class Properties {
 	private String discordAuthUrl = "https://discordapp.com/api/auth";
 	private String discordChannelsUrl = "https://discordapp.com/api/channels";
 	private String userAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121202 Firefox/17.0 Iceweasel/17.0.1";
+	
+	private final Path iniPath = Paths.get(Paths.get(System.getProperty("user.home"), ".accord").toString(),"accord.ini");
+	
+	public Path getIniPath() {
+		return iniPath;
+	}
 	
 	public String getDiscordUsersUrl() {
 		return discordUsersUrl;
