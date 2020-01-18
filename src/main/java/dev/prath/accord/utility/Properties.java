82f035c0e60a3beb7aadae5880eb7abd68855930
Aaron Prather <34904535+aaprather@users.noreply.github.com>
2020-01-18 18:55:02 -0500

    Change Properties to be ununitializable, instead call static properties

--- src/main/java/dev/prath/accord/utility/Properties.java
+++ src/main/java/dev/prath/accord/utility/Properties.java
@@ -5,35 +5,12 @@ import java.nio.file.Paths;
 
 public class Properties {
 	
-	private String discordUsersUrl = "https://discordapp.com/api/users";
-	private String discordGuildsUrl = "https://discordapp.com/api/guilds";
-	private String discordAuthUrl = "https://discordapp.com/api/auth";
-	private String discordChannelsUrl = "https://discordapp.com/api/channels";
-	private String userAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121202 Firefox/17.0 Iceweasel/17.0.1";
-	
-	private final Path iniPath = Paths.get(Paths.get(System.getProperty("user.home"), ".accord").toString(),"accord.ini");
-	
-	public Path getIniPath() {
-		return iniPath;
-	}
-	
-	public String getDiscordUsersUrl() {
-		return discordUsersUrl;
-	}
-
-	public String getDiscordGuildsUrl() {
-		return discordGuildsUrl;
-	}
-	public String getDiscordAuthUrl() {
-		return discordAuthUrl;
-	}
-	
-	public String getDiscordChannelsUrl() {
-		return discordChannelsUrl;
-	}
-	public String getUserAgent() {
-		return userAgent;
-	}
+	public static final String discordUsersUrl = "https://discordapp.com/api/users";
+	public static final String discordGuildsUrl = "https://discordapp.com/api/guilds";
+	public static final String discordAuthUrl = "https://discordapp.com/api/auth";
+	public static final String discordChannelsUrl = "https://discordapp.com/api/channels";
+	public static final String userAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121202 Firefox/17.0 Iceweasel/17.0.1";
+	public static final Path iniPath = Paths.get(Paths.get(System.getProperty("user.home"), ".accord").toString(),"accord.ini");
 	
 }
 
