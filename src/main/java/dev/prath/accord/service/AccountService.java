3a7bcdb7475d6c121b886ed5bf8a1dbccce74719
Aaron Prather <34904535+aaprather@users.noreply.github.com>
2020-01-25 22:52:17 -0500

    Add the attachReferences function in AccountService, this is used to replace any raw references to other users with the actual username (if available)

--- src/main/java/dev/prath/accord/service/AccountService.java
+++ src/main/java/dev/prath/accord/service/AccountService.java
@@ -1,5 +1,10 @@
 package dev.prath.accord.service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -7,6 +12,8 @@ import org.springframework.stereotype.Service;
 import dev.prath.accord.domain.Channel;
 import dev.prath.accord.domain.Conversation;
 import dev.prath.accord.domain.DiscordAccount;
+import dev.prath.accord.domain.Message;
+import dev.prath.accord.domain.User;
 
 @Service
 public class AccountService {
@@ -21,34 +28,60 @@ public class AccountService {
 	}
 
 	public void setSelectedConversation(Conversation val) {
+		attachReferences(val.getMessages(), val.getRecipients());
 		selectedConversation = val;
-		logger.info("AccountService has changed the selected conversation from "
+		logger.warn("AccountService has changed the selected conversation from "
 				+ (selectedConversation != null ? selectedConversation.getId() : "[Empty]") + " to " + val.getId());
 	}
 
 	public Conversation getSelectedConversation() {
-		logger.info("AccountService is returning the selected conversation");
 		return selectedConversation;
 	}
 
 	public void setSelectedChannel(Channel val) {
+		attachReferences(val.getMessages(), val.getParticipatingUsers());
 		selectedChannel = val;
-		logger.info("AccountService has changed the selected channel from "
+		logger.warn("AccountService has changed the selected channel from "
 				+ (selectedChannel != null ? selectedChannel.getId() : "[Empty]") + " to " + val.getId());
 	}
 
 	public Channel getSelectedChannel() {
-		logger.info("AccountService is returning the selected channel");
 		return selectedChannel;
 	}
 
 	public DiscordAccount getDiscordAccount() {
-		logger.info("AccountService is returning the discord account");
 		return discordAccount;
 	}
 
 	public void updateDiscordAccount(DiscordAccount val) {
 		discordAccount = val;
-		logger.info("AccountService has updated the discord account");
+		logger.warn("AccountService has updated the discord account");
+	}
+
+	private void attachReferences(List<Message> messages, List<User> users) {
+		messages.stream().forEach(m -> m.setMessage(m.getMessage().replaceAll("<@!", "<@")));
+		for (Message msg : messages) {
+			String content = msg.getMessage();
+
+			var rawUserIds = StringUtils.substringsBetween(content, "<@", ">");
+
+			if (rawUserIds != null) {
+				var distinctIds = Arrays.stream(rawUserIds).distinct().collect(Collectors.toList());
+
+				for (String id : distinctIds) {
+					User matchedUser = null;
+					try {
+						matchedUser = users.stream().filter(user -> user.getId().equalsIgnoreCase(id))
+								.collect(Collectors.toList()).get(0);
+					} catch (Exception e) {
+						content = content.replaceAll("<@" + id + ">", "@[USER: " + id + "]");
+					}
+					if (matchedUser != null) {
+						content = content.replaceAll("<@" + id + ">", "@" + matchedUser.getUsername());
+					}
+				}
+				msg.setMessage(content);
+			}
+		}
 	}
 }
