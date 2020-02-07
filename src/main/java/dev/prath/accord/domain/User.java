c3494f8bd04c633edd41302ab7b6d1f519389724
Aaron <aprathdev@gmail.com>
2020-02-06 22:29:37 -0500

    Add isSelected property to User domain class

--- src/main/java/dev/prath/accord/domain/User.java
+++ src/main/java/dev/prath/accord/domain/User.java
@@ -3,6 +3,9 @@ package dev.prath.accord.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
 	@JsonProperty("id")
@@ -10,6 +13,16 @@ public class User {
 
 	@JsonProperty("username")
 	private String username = "";
+	
+	private BooleanProperty isSelected = new SimpleBooleanProperty();
+	
+	public final BooleanProperty getIsSelected() {
+		return isSelected;
+	}
+
+	public final void setIsSelected(final boolean val) {
+		getIsSelected().set(val);
+	}
 	
 	public String getId() {
 		return id;
