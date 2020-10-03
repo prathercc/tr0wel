3974329b08658f04db342bff32ea95b02d887d21
Aaron <aprathdev@gmail.com>
2020-10-03 12:51:01 -0700

    Create LoadingBox fxml and controller

---
+++ src/main/java/cc/prather/tr0wel/controller/utility/LoadingBoxController.java
@@ -0,0 +1,12 @@
+package cc.prather.tr0wel.controller.utility;
+
+import org.springframework.stereotype.Component;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+@Component
+public class LoadingBoxController {
+	@FXML
+	private Text loadingText;
+}
